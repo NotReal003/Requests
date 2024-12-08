d1422dfc424217b5f6c15cc69db47192b8e04bc4
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-12-08 20:08:06 +0530

    Update Analytics.jsx

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -11,7 +11,7 @@ const Analytics = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://request.notreal003.xyz/api/collect/visits', {
+        const response = await fetch('https://request.notreal003.xyz/api/collect/check', {
           withCredentials: true,
         });
         const data = await response.json();
