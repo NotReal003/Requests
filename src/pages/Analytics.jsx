768a5928ea5d2ece23954298eab1f147c4c4090e
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-12-24 12:51:43 +0000

    Fix

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -114,7 +114,7 @@ const Analytics = () => {
                     <Tooltip />
                     <Line
                       type="monotone"
-                      dataKey='visits'
+                      dataKey="visits"
                       stroke="#4ADE80"
                       strokeWidth={2}
                       dot={{ r: 4 }}
