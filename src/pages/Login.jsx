c489a1220df32de9b5f17a6257ee099377990705
NotReal <117504040+NotReal003@users.noreply.github.com>
2025-01-19 09:38:17 +0000

    Fix

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
   }, []);
 
   const handleLogin = () => {
-    const authWindow = window.open('about:blank');
+    const authWindow = window.open('about:blank', '_blank',);
     setTimeout(() => {
       authWindow.location.href = `${API}/auth/signin`;
     }, 1000);
