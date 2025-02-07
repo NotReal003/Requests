9a6d7cd425b58aaef0b01ce47be08cef39e0f202
NotReal <117504040+NotReal003@users.noreply.github.com>
2025-02-07 21:42:21 +0530

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -74,7 +74,8 @@ const Profile = () => {
       <div className="flex items-center space-x-4 mb-8">
         {user && user.avatarHash ? (
           <img
-            src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatarHash}.webp?size=128`}
+            src={`${user.avatarHash}`}
+            //src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatarHash}.webp?size=128`}
             className="ring-primary ring-offset-base-100 w-24 h-24 rounded-full ring-4 ring-offset-2 shadow-lg"
             alt={user.username}
           />
