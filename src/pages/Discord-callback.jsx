bb94319b9738a6aa8f418937954323d5335275b9
NotReal <117504040+NotReal003@users.noreply.github.com>
2025-02-10 20:39:43 +0530

    Update Discord-callback.jsx

--- src/pages/Discord-callback.jsx
+++ src/pages/Discord-callback.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { FaSpinner, FaLock } from "react-icons/fa";
+import { FaSpinner, FaDiscord } from "react-icons/fa";
 import { FcLock } from "react-icons/fc";
 import toast, { Toaster } from 'react-hot-toast';
 
