173c1283ea372645ccc114a06792d164b27e8445
NotReal <117504040+NotReal003@users.noreply.github.com>
2025-02-15 12:21:53 +0530

    Update AdminOnly.jsx

--- src/components/AdminOnly.jsx
+++ src/components/AdminOnly.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PiDetectiveFill } from "react-icons/pi";
+import { FaUserSecret } from "react-icons/fa";
 
 
 const AdminOnly = () => {
@@ -7,7 +7,7 @@ const AdminOnly = () => {
     <div className="flex flex-col items-center justify-center h-screen bg-base-100 text-white">
       <div className="text-center">
         <div className="mb-4">
-          <PiDetectiveFill className="w-16 h-16 mx-auto text-gray-500" />
+          <FaUserSecret className="w-16 h-16 mx-auto text-gray-500" />
         </div>
         <h1 className="text-3xl font-bold">This Page Isn't Meant for You</h1>
         <p className="text-gray-400 mt-2">
