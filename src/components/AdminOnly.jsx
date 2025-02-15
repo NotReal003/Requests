0b076fbb6674b2780b62baa26c90bbc5bc24141a
NotReal <117504040+NotReal003@users.noreply.github.com>
2025-02-15 12:32:16 +0530

    Update AdminOnly.jsx

--- src/components/AdminOnly.jsx
+++ src/components/AdminOnly.jsx
@@ -4,10 +4,10 @@ import { FaUserSecret } from "react-icons/fa";
 
 const AdminOnly = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-base-100 text-white">
+    <div className="flex flex-col items-center justify-center p-4 h-screen bg-base-100 text-white">
       <div className="text-center">
         <div className="mb-4">
-          <FaUserSecret className="w-16 h-16 mx-auto text-gray-500" />
+          <FaUserSecret className="w-16 h-16 mx-auto text-gray-400" />
         </div>
         <h1 className="text-3xl font-bold">This Page Isn't Meant for You</h1>
         <p className="text-gray-400 mt-2">
